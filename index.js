a04cf2b263c63774236bc647bfcc667ec8297607
asaha448 <asacodes4@gmail.com>
2024-02-20 22:09:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,552 @@
+var magnifyq=document.getElementById("qkey");
+var magnifyw=document.getElementById("wkey");
+var magnifye=document.getElementById("ekey");
+var magnifyr=document.getElementById("rkey");
+var magnifyt=document.getElementById("tkey");
+var magnifyy=document.getElementById("ykey");
+var magnifyu=document.getElementById("ukey");
+var magnifyi=document.getElementById("ikey");
+var magnifyo=document.getElementById("okey");
+var magnifyp=document.getElementById("pkey");
+
+var magnifya=document.getElementById("akey");
+var magnifys=document.getElementById("skey");
+var magnifyd=document.getElementById("dkey");
+var magnifyf=document.getElementById("fkey");
+var magnifyg=document.getElementById("gkey");
+var magnifyh=document.getElementById("hkey");
+var magnifyj=document.getElementById("jkey");
+var magnifyk=document.getElementById("kkey");
+var magnifyl=document.getElementById("lkey");
+
+var magnifyz=document.getElementById("zkey");
+var magnifyx=document.getElementById("xkey");
+var magnifyc=document.getElementById("ckey");
+var magnifyv=document.getElementById("vkey");
+var magnifyb=document.getElementById("bkey");
+var magnifyn=document.getElementById("nkey");
+var magnifym=document.getElementById("mkey");
+
+
+document.body.addEventListener('keydown', function(e) {
+    var keyC = e.key;
+
+    if(keyC === "q") {
+        magnifyq.style.fontSize = "40px";
+        magnifyq.style.opacity="0.7";
+        magnifyq.style.borderColor="lightyellow"
+        magnifyq.style.borderWidth="2px"
+        magnifyq.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioQ");
+        playAudioQ()
+    }
+    if(keyC === "w") {
+        magnifyw.style.fontSize = "40px";
+        magnifyw.style.opacity="0.7";
+        magnifyw.style.borderColor="lightyellow"
+        magnifyw.style.borderWidth="2px"
+        magnifyw.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioW");
+        playAudioW()
+    }
+    if(keyC === "e") {
+        magnifye.style.fontSize = "40px";
+        magnifye.style.opaqity="0.7";
+        magnifye.style.borderColor="lightyellow"
+        magnifye.style.borderWidth="2px"
+        magnifye.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioE");
+        playAudioE()
+    }
+    if(keyC === "r") {
+        magnifyr.style.fontSize = "40px";
+        magnifyr.style.opacity="0.7";
+        magnifyr.style.borderColor="lightyellow"
+        magnifyr.style.borderWidth="2px"
+        magnifyr.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioR");
+        playAudioR()
+    }
+    if(keyC === "t") {
+        magnifyt.style.fontSize = "40px";
+        magnifyt.style.opacity="0.7";
+        magnifyt.style.borderColor="lightyellow"
+        magnifyt.style.borderWidth="2px"
+        magnifyt.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioT");
+        playAudioT()
+    }
+    if(keyC === "y") {
+        magnifyy.style.fontSize = "40px";
+        magnifyy.style.opacity="0.7";
+        magnifyy.style.borderColor="lightyellow"
+        magnifyy.style.borderWidth="2px"
+        magnifyy.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioY");
+        playAudioY()
+    }
+    if(keyC === "u") {
+        magnifyu.style.fontSize = "40px";
+        magnifyu.style.opacity="0.7";
+        magnifyu.style.borderColor="lightyellow"
+        magnifyu.style.borderWidth="2px"
+        magnifyu.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioU");
+        playAudioU()
+    }
+    if(keyC === "i") {
+        magnifyi.style.fontSize = "40px";
+        magnifyi.style.opacity="0.7";
+        magnifyi.style.borderColor="lightyellow"
+        magnifyi.style.borderWidth="2px"
+        magnifyi.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioI");
+        playAudioI()
+    }
+    if(keyC === "o") {
+        magnifyo.style.fontSize = "40px";
+        magnifyo.style.opacity="0.7";
+        magnifyo.style.borderColor="lightyellow"
+        magnifyo.style.borderWidth="2px"
+        magnifyo.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioO");
+        playAudioO()
+    }
+    if(keyC === "p") {
+        magnifyp.style.fontSize = "40px";
+        magnifyp.style.opacity="0.7";
+        magnifyp.style.borderColor="lightyellow"
+        magnifyp.style.borderWidth="2px"
+        magnifyp.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioP");
+        playAudioP()
+    }
+
+
+
+    if(keyC === "a") {
+        magnifya.style.fontSize = "40px";
+        magnifya.style.opacity="0.7";
+        magnifya.style.borderColor="lightyellow"
+        magnifya.style.borderWidth="2px"
+        magnifya.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioA");
+        playAudioA()
+    }
+    if(keyC === "s") {
+        magnifys.style.fontSize = "40px";
+        magnifys.style.opacity="0.7";
+        magnifys.style.borderColor="lightyellow"
+        magnifys.style.borderWidth="2px"
+        magnifys.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioS");
+        playAudioS()
+    }
+    if(keyC === "d") {
+        magnifyd.style.fontSize = "40px";
+        magnifyd.style.opacity="0.7";
+        magnifyd.style.borderColor="lightyellow"
+        magnifyd.style.borderWidth="2px"
+        magnifyd.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioD");
+        playAudioD()
+    }
+    if(keyC === "f") {
+        magnifyf.style.fontSize = "40px";
+        magnifyf.style.opacity="0.7";
+        magnifyf.style.borderColor="lightyellow"
+        magnifyf.style.borderWidth="2px"
+        magnifyf.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioF");
+        playAudioF()
+    }
+    if(keyC === "g") {
+        magnifyg.style.fontSize = "40px";
+        magnifyg.style.opacity="0.7";
+        magnifyg.style.borderColor="lightyellow"
+        magnifyg.style.borderWidth="2px"
+        magnifyg.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioG");
+        playAudioG()
+    }
+    if(keyC === "h") {
+        magnifyh.style.fontSize = "40px";
+        magnifyh.style.opacity="0.7";
+        magnifyh.style.borderColor="lightyellow"
+        magnifyh.style.borderWidth="2px"
+        magnifyh.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioH");
+        playAudioH()
+    }
+    if(keyC === "j") {
+        magnifyj.style.fontSize = "40px";
+        magnifyj.style.opacity="0.7";
+        magnifyj.style.borderColor="lightyellow"
+        magnifyj.style.borderWidth="2px"
+        magnifyj.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioJ");
+        playAudioJ()
+    }
+    if(keyC === "k") {
+        magnifyk.style.fontSize = "40px";
+        magnifyk.style.opacity="0.7";
+        magnifyk.style.borderColor="lightyellow"
+        magnifyk.style.borderWidth="2px"
+        magnifyk.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioK");
+        playAudioK()
+    }
+    if(keyC === "l") {
+        magnifyl.style.fontSize = "40px";
+        magnifyl.style.opacity="0.7";
+        magnifyl.style.borderColor="lightyellow"
+        magnifyl.style.borderWidth="2px"
+        magnifyl.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioL");
+        playAudioL()
+    }
+
+
+
+    if(keyC === "z") {
+        magnifyz.style.fontSize = "40px";
+        magnifyz.style.opacity="0.7";
+        magnifyz.style.borderColor="lightyellow"
+        magnifyz.style.borderWidth="2px"
+        magnifyz.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioZ");
+        playAudioZ()
+    }
+    if(keyC === "x") {
+        magnifyx.style.fontSize = "40px";
+        magnifyx.style.opacity="0.7";
+        magnifyx.style.borderColor="lightyellow"
+        magnifyx.style.borderWidth="2px"
+        magnifyx.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioX");
+        playAudioX()
+    }
+    if(keyC === "c") {
+        magnifyc.style.fontSize = "40px";
+        magnifyc.style.opacity="0.7";
+        magnifyc.style.borderColor="lightyellow"
+        magnifyc.style.borderWidth="2px"
+        magnifyc.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioC");
+        playAudioC()
+    }
+    if(keyC === "v") {
+        magnifyv.style.fontSize = "40px";
+        magnifyv.style.opacity="0.7";
+        magnifyv.style.borderColor="lightyellow"
+        magnifyv.style.borderWidth="2px"
+        magnifyv.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioV");
+        playAudioV()
+    }
+    if(keyC === "b") {
+        magnifyb.style.fontSize = "40px";
+        magnifyb.style.opacity="0.7";
+        magnifyb.style.borderColor="lightyellow"
+        magnifyb.style.borderWidth="2px"
+        magnifyb.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioB");
+        playAudioB()
+    }
+    if(keyC === "n") {
+        magnifyn.style.fontSize = "40px";
+        magnifyn.style.opacity="0.7";
+        magnifyn.style.borderColor="lightyellow"
+        magnifyn.style.borderWidth="2px"
+        magnifyn.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioN");
+        playAudioN()
+    }
+    if(keyC === "m") {
+        magnifym.style.fontSize = "40px";
+        magnifym.style.opacity="0.7";
+        magnifym.style.borderColor="lightyellow"
+        magnifym.style.borderWidth="2px"
+        magnifym.style.boxShadow= "2px 2px lightblue"
+        var x = document.getElementById("myAudioM");
+        playAudioM()
+    }
+
+
+    function playAudioQ() {
+        x.play();
+    }
+    function playAudioW() {
+        x.play();
+    }
+    function playAudioE() {
+        x.play();
+    }
+    function playAudioR() {
+        x.play();
+    }
+    function playAudioT() {
+        x.play();
+    }
+    function playAudioY() {
+        x.play();
+    }
+    function playAudioU() {
+        x.play();
+    }
+    function playAudioI() {
+        x.play();
+    }
+    function playAudioO() {
+        x.play();
+    }
+    function playAudioP() {
+        x.play();
+    }
+
+
+    function playAudioA() {
+        x.play();
+    }
+    function playAudioS() {
+        x.play();
+    }
+    function playAudioD() {
+        x.play();
+    }
+    function playAudioF() {
+        x.play();
+    }
+    function playAudioG() {
+        x.play();
+    }
+    function playAudioH() {
+        x.play();
+    }
+    function playAudioJ() {
+        x.play();
+    }
+    function playAudioK() {
+        x.play();
+    }
+    function playAudioL() {
+        x.play();
+    }
+
+
+    function playAudioZ() {
+        x.play();
+    }
+    function playAudioX() {
+        x.play();
+    }
+    function playAudioC() {
+        x.play();
+    }
+    function playAudioV() {
+        x.play();
+    }
+    function playAudioG() {
+        x.play();
+    }
+    function playAudioB() {
+        x.play();
+    }
+    function playAudioN() {
+        x.play();
+    }
+    function playAudioM() {
+        x.play();
+    }
+});
+document.body.addEventListener('keyup', function(e) {
+    var keyU = e.key;
+
+    if(keyU === "q" ) {
+        magnifyq.style.fontSize = "30px";
+        magnifyq.style.opacity="0.4";
+        magnifyq.style.borderColor="black"
+        magnifyq.style.borderWidth="4px"
+        magnifyq.style.boxShadow= "none"
+    }
+    if(keyU === "w" ) {
+        magnifyw.style.fontSize = "30px";
+        magnifyw.style.opacity="0.4";
+        magnifyw.style.borderColor="black"
+        magnifyw.style.borderWidth="4px"
+        magnifyw.style.boxShadow= "none"
+    }
+    if(keyU === "e" ) {
+        magnifye.style.fontSize = "30px";
+        magnifye.style.opacity="0.4";
+        magnifye.style.borderColor="black"
+        magnifye.style.borderWidth="4px"
+        magnifye.style.boxShadow= "none"
+    }
+    if(keyU === "r" ) {
+        magnifyr.style.fontSize = "30px";
+        magnifyr.style.opacity="0.4";
+        magnifyr.style.borderColor="black"
+        magnifyr.style.borderWidth="4px"
+        magnifyr.style.boxShadow= "none"
+    }
+    if(keyU === "t" ) {
+        magnifyt.style.fontSize = "30px";
+        magnifyt.style.opacity="0.4";
+        magnifyt.style.borderColor="black"
+        magnifyt.style.borderWidth="4px"
+        magnifyt.style.boxShadow= "none"
+    }
+    if(keyU === "y" ) {
+        magnifyy.style.fontSize = "30px";
+        magnifyy.style.opacity="0.4";
+        magnifyy.style.borderColor="black"
+        magnifyy.style.borderWidth="4px"
+        magnifyy.style.boxShadow= "none"
+    }
+    if(keyU === "u" ) {
+        magnifyu.style.fontSize = "30px";
+        magnifyu.style.opacity="0.4";
+        magnifyu.style.borderColor="black"
+        magnifyu.style.borderWidth="4px"
+        magnifyu.style.boxShadow= "none"
+    }
+    if(keyU === "i" ) {
+        magnifyi.style.fontSize = "30px";
+        magnifyi.style.opacity="0.4";
+        magnifyi.style.borderColor="black"
+        magnifyi.style.borderWidth="4px"
+        magnifyi.style.boxShadow= "none"
+    }
+    if(keyU === "o" ) {
+        magnifyo.style.fontSize = "30px";
+        magnifyo.style.opacity="0.4";
+        magnifyo.style.borderColor="black"
+        magnifyo.style.borderWidth="4px"
+        magnifyo.style.boxShadow= "none"
+    }
+    if(keyU === "p" ) {
+        magnifyp.style.fontSize = "30px";
+        magnifyp.style.opacity="0.4";
+        magnifyp.style.borderColor="black"
+        magnifyp.style.borderWidth="4px"
+        magnifyp.style.boxShadow= "none"
+    }
+
+
+    if(keyU === "a" ) {
+        magnifya.style.fontSize = "30px";
+        magnifya.style.opacity="0.4";
+        magnifya.style.borderColor="black"
+        magnifya.style.borderWidth="4px"
+        magnifya.style.boxShadow= "none"
+    }
+    if(keyU === "s" ) {
+        magnifys.style.fontSize = "30px";
+        magnifys.style.opacity="0.4";
+        magnifys.style.borderColor="black"
+        magnifys.style.borderWidth="4px"
+        magnifys.style.boxShadow= "none"
+    }
+    if(keyU === "d" ) {
+        magnifyd.style.fontSize = "30px";
+        magnifyd.style.opacity="0.4";
+        magnifyd.style.borderColor="black"
+        magnifyd.style.borderWidth="4px"
+        magnifyd.style.boxShadow= "none"
+    }
+    if(keyU === "f" ) {
+        magnifyf.style.fontSize = "30px";
+        magnifyf.style.opacity="0.4";
+        magnifyf.style.borderColor="black"
+        magnifyf.style.borderWidth="4px"
+        magnifyf.style.boxShadow= "none"
+    }
+    if(keyU === "g" ) {
+        magnifyg.style.fontSize = "30px";
+        magnifyg.style.opacity="0.4";
+        magnifyg.style.borderColor="black"
+        magnifyg.style.borderWidth="4px"
+        magnifyg.style.boxShadow= "none"
+    }
+    if(keyU === "h" ) {
+        magnifyh.style.fontSize = "30px";
+        magnifyh.style.opacity="0.4";
+        magnifyh.style.borderColor="black"
+        magnifyh.style.borderWidth="4px"
+        magnifyh.style.boxShadow= "none"
+    }
+    if(keyU === "j" ) {
+        magnifyj.style.fontSize = "30px";
+        magnifyj.style.opacity="0.4";
+        magnifyj.style.borderColor="black"
+        magnifyj.style.borderWidth="4px"
+        magnifyj.style.boxShadow= "none"
+    }
+    if(keyU === "k" ) {
+        magnifyk.style.fontSize = "30px";
+        magnifyk.style.opacity="0.4";
+        magnifyk.style.borderColor="black"
+        magnifyk.style.borderWidth="4px"
+        magnifyk.style.boxShadow= "none"
+    }
+    if(keyU === "l" ) {
+        magnifyl.style.fontSize = "30px";
+        magnifyl.style.opacity="0.4";
+        magnifyl.style.borderColor="black"
+        magnifyl.style.borderWidth="4px"
+        magnifyl.style.boxShadow= "none"
+    }
+
+
+    if(keyU === "z" ) {
+        magnifyz.style.fontSize = "30px";
+        magnifyz.style.opacity="0.4";
+        magnifyz.style.borderColor="black"
+        magnifyz.style.borderWidth="4px"
+        magnifyz.style.boxShadow= "none"
+    }
+    if(keyU === "x" ) {
+        magnifyx.style.fontSize = "30px";
+        magnifyx.style.opacity="0.4";
+        magnifyx.style.borderColor="black"
+        magnifyx.style.borderWidth="4px"
+        magnifyx.style.boxShadow= "none"
+    }
+    if(keyU === "c" ) {
+        magnifyc.style.fontSize = "30px";
+        magnifyc.style.opacity="0.4";
+        magnifyc.style.borderColor="black"
+        magnifyc.style.borderWidth="4px"
+        magnifyc.style.boxShadow= "none"
+    }
+    if(keyU === "v" ) {
+        magnifyv.style.fontSize = "30px";
+        magnifyv.style.opacity="0.4";
+        magnifyv.style.borderColor="black"
+        magnifyv.style.borderWidth="4px"
+        magnifyv.style.boxShadow= "none"
+    }
+    if(keyU === "b" ) {
+        magnifyb.style.fontSize = "30px";
+        magnifyb.style.opacity="0.4";
+        magnifyb.style.borderColor="black"
+        magnifyb.style.borderWidth="4px"
+        magnifyb.style.boxShadow= "none"
+    }
+    if(keyU === "n" ) {
+        magnifyn.style.fontSize = "30px";
+        magnifyn.style.opacity="0.4";
+        magnifyn.style.borderColor="black"
+        magnifyn.style.borderWidth="4px"
+        magnifyn.style.boxShadow= "none"
+    }
+    if(keyU === "m" ) {
+        magnifym.style.fontSize = "30px";
+        magnifym.style.opacity="0.4";
+        magnifym.style.borderColor="black"
+        magnifym.style.borderWidth="4px"
+        magnifym.style.boxShadow= "none"
+    }
+
+});
